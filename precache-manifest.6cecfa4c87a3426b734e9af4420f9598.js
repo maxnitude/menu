15a18cc7d7b16b53ff284eef81f1f1fb0b48eeb3
maxnitude <mailbox.timofeev@gmail.com>
2021-03-08 22:31:03 +0300

    Updates

---
+++ precache-manifest.6cecfa4c87a3426b734e9af4420f9598.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc48a8542ea584d840d3fceab36a05a2",
+    "url": "/menu/index.html"
+  },
+  {
+    "revision": "51edb8eafaf42e533477",
+    "url": "/menu/static/css/main.d97ac367.chunk.css"
+  },
+  {
+    "revision": "6126af699e2b11863f3d",
+    "url": "/menu/static/js/2.c86ad9b2.chunk.js"
+  },
+  {
+    "revision": "51edb8eafaf42e533477",
+    "url": "/menu/static/js/main.e790efa6.chunk.js"
+  },
+  {
+    "revision": "358a7b4e210680b2448a",
+    "url": "/menu/static/js/runtime~main.4e21075e.js"
+  },
+  {
+    "revision": "701d6d1d57f5d528c4db1e887ff29e82",
+    "url": "/menu/static/media/shopping-cart-solid.701d6d1d.svg"
+  }
+]);
